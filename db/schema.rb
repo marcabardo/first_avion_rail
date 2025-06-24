ecc977ccda1452d211af45b4f7e5d5ae1f910c3c
marcabardo <cabardomararvel@gmail.com>
2025-06-24 15:41:52 +0800

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_20_171646) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_24_071442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,11 +19,22 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_20_171646) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "classrooms", id: :serial, force: :cascade do |t|
     t.integer "student_id"
     t.string "section", limit: 10
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "roles", id: :serial, force: :cascade do |t|
@@ -53,6 +64,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_20_171646) do
   end
 
   add_foreign_key "classrooms", "students", name: "fk_student"
+  add_foreign_key "comments", "articles"
   add_foreign_key "users_roles", "roles", name: "fk_roles"
   add_foreign_key "users_roles", "users", name: "fk_users"
 end
